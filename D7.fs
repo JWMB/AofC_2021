5d0d356bf4d64d7066106d156247b5e6542345d1
Jonas Beckeman <jonas.beckeman@gmail.com>
2021-12-07 06:44:10 +0100

    D7 pt 1

---
+++ D7.fs
@@ -0,0 +1,14 @@
+﻿module D7
+
+open System.Text.RegularExpressions
+
+let main =
+    let inputX = "16,1,2,0,4,2,7,1,2,14"
+
+    let input = System.IO.File.ReadAllText("D7.txt")
+
+    let data = Regex.Split(input.Trim(), @",") |> Array.map int
+    //data |> Array.map (fun f -> (float f)) |>Array.average
+    let sorted = data |> Array.sort
+    let median = sorted.[sorted.Length / 2]
+    sorted |> Array.map (fun f -> abs(f - median)) |> Array.sum
