083f9885636a4cc1c6d1dc8cc87bdf0bffa97244
Jonas Beckeman <jonas.beckeman@gmail.com>
2021-12-07 11:14:13 +0100

    "part"

--- D7.fs
+++ D7.fs
@@ -9,11 +9,11 @@ let main =
 
     let data = Regex.Split(input.Trim(), @",") |> Array.map int
     
-    let costFunctionDay1 p1 p2 = abs(p1 - p2)
-    let costFunctionDay2 p1 p2 = 
+    let costFunctionPart1 p1 p2 = abs(p1 - p2)
+    let costFunctionPart2 p1 p2 = 
         let diff = abs(p1 - p2)
         diff * (diff + 1) / 2
-    let costFunction = costFunctionDay2
+    let costFunction = costFunctionPart2
 
     let allPositions = [| data |> Array.min .. data |> Array.max |]
     let costs = allPositions |> Array.map (fun f -> {| Position = f; Cost = data |> Array.map (fun f2 -> costFunction f f2) |> Array.sum |})
