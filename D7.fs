57fe291fa53f0755d0a066cd37fdd2a8cd129b59
Jonas Beckeman <jonas.beckeman@gmail.com>
2021-12-07 11:05:35 +0100

    D7

--- D7.fs
+++ D7.fs
@@ -3,12 +3,20 @@
 open System.Text.RegularExpressions
 
 let main =
-    let inputX = "16,1,2,0,4,2,7,1,2,14"
+    let input = "16,1,2,0,4,2,7,1,2,14"
 
     let input = System.IO.File.ReadAllText("D7.txt")
 
     let data = Regex.Split(input.Trim(), @",") |> Array.map int
-    //data |> Array.map (fun f -> (float f)) |>Array.average
-    let sorted = data |> Array.sort
-    let median = sorted.[sorted.Length / 2]
-    sorted |> Array.map (fun f -> abs(f - median)) |> Array.sum
+    
+    let costFunctionDay1 p1 p2 = abs(p1 - p2)
+    let costFunctionDay2 p1 p2 = 
+        let diff = abs(p1 - p2)
+        diff * (diff + 1) / 2
+    let costFunction = costFunctionDay2
+
+    let allPositions = [| data |> Array.min .. data |> Array.max |]
+    let costs = allPositions |> Array.map (fun f -> {| Position = f; Cost = data |> Array.map (fun f2 -> costFunction f f2) |> Array.sum |})
+    // printf "%O" (costs |> Array.map (fun f -> $"{f.Position}:{f.Cost}") |> String.concat "\n")
+    let itemWithMinCost = costs |> Array.minBy (fun f -> f.Cost)
+    itemWithMinCost.Cost
