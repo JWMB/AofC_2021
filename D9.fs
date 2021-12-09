433de21c06aca77b3cde66e6ae4c50bcf765f022
Jonas Beckeman <jonas.beckeman@gmail.com>
2021-12-09 11:35:27 +0100

    D9 pt 2

--- D9.fs
+++ D9.fs
@@ -23,13 +23,65 @@ let main =
     let getAtPoint pt =
         int (data.[pt.Y].[pt.X].ToString())
 
-    let isLowPoint pt =
+    let getSurrounding pt = 
         let offsets = [| 1; -1; |] |> Array.map (fun f -> [| { X = f * 1; Y = 0;}; { X = 0; Y = f * 1;}|]) |> Array.reduce Array.append
         let isValid pt = pt.X >= 0 && pt.X < size.X && pt.Y >= 0 && pt.Y < size.Y
-        let samplePts = offsets |> Array.map (fun f -> add pt f) |> Array.filter isValid
-        let sampleVals = samplePts |> Array.map getAtPoint
+        offsets |> Array.map (fun f -> add pt f) |> Array.filter isValid
+
+    let isLowPoint pt =
+        let sampleVals = getSurrounding pt |> Array.map getAtPoint
         sampleVals |> Array.min > getAtPoint pt
 
-    let allPoints = [|0..size.X - 1|] |> Array.map (fun x -> [|0..size.Y - 1|] |> Array.map (fun y -> { X = x; Y = y; })) |> Array.reduce Array.append
+    let allPointsNested = [|0..size.Y - 1|] |> Array.map (fun y -> [|0..size.X - 1|] |> Array.map (fun x -> { X = x; Y = y; }))
+    let allPoints = allPointsNested |> Array.reduce Array.append
 
-    allPoints |> Array.filter isLowPoint |> Array.map getAtPoint |> Array.map (fun f -> f + 1) |> Array.sum
+    let lowPoints = allPoints |> Array.filter isLowPoint
+
+    // part 2:
+    let exploreBasin ptStart =
+        [|0..1000|] |> Array.fold (fun (agg: {| Explored: Point []; InBasin: Point []; ToExplore: Point [] |}) curr -> 
+                        if agg.ToExplore.Length = 0 then agg
+                        else
+                        let pt = agg.ToExplore |> Array.head
+                        let newUnexplored = getSurrounding pt |> Array.except agg.ToExplore |> Array.except agg.Explored
+                        let byStopping = Map (newUnexplored |> Array.groupBy (fun f -> getAtPoint f = 9)) // Hm, works but only b/c all basins happen to be surrounded by 9's
+                        let getMapValueElseEmpty key = if byStopping.ContainsKey key then byStopping.[key] else Array.zeroCreate 0
+                        {| 
+                            ToExplore = agg.ToExplore |> Array.tail |> Array.append (getMapValueElseEmpty false);
+                            Explored = agg.Explored |> Array.append [| pt|] |> Array.append (getMapValueElseEmpty true);
+                            InBasin = agg.InBasin |> Array.append [| pt|]
+                        |}
+                    ) {| ToExplore = [| ptStart; |]; Explored = Array.zeroCreate<Point> 0; InBasin = Array.zeroCreate<Point> 0; |}
+
+    let basinsBySize = lowPoints |> Array.map exploreBasin |> Array.map (fun f -> f.InBasin) |> Array.sortBy (fun f -> f.Length) |> Array.rev
+    let ptToBasin = Map (basinsBySize |> Array.mapi (fun i f -> f |> Array.map (fun p -> (p, i))) |> Array.reduce Array.append)
+    
+    let renderAsHtml numToColor = 
+        let css = basinsBySize |> Array.mapi (fun i f -> 
+            $".b{string i} {{ background-color: hsl({string (i * 360 / basinsBySize.Length)}, 100#, {if i < numToColor then 70 else 90}#); }}".Replace("#", "%")) 
+                               |> String.concat "\n"
+
+        let createElements row = row |> Array.map (fun p -> 
+                                        let value = getAtPoint p
+                                        match ptToBasin.TryFind(p) with
+                                        | Some x -> $"<span class='b{x}'>{value}</span>"
+                                        | None -> $"{value}"
+                                    ) |> String.concat "\n"
+
+        let elements = allPointsNested |> Array.map createElements |> String.concat "<br/>"
+
+        $"<!DOCTYPE html><html>
+        <head>
+            <title>Basins!</title>
+            <style>{css}</style>
+        </head>
+        <body>{elements}</body>
+        </html> "
+    
+    System.IO.File.WriteAllText(@"C:\Users\jonas\Desktop\Basins.html", renderAsHtml 3);
+
+    let top3 = basinsBySize |> Array.take 3
+    top3 |> Array.map (fun f -> f.Length) |> Array.reduce (fun agg curr -> agg * curr)
+
+    // part 1:
+    //lowPoints |> Array.map getAtPoint |> Array.map (fun f -> f + 1) |> Array.sum
