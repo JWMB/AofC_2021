dc9e589c9c391356aeb7ef32260ea9ceb4849bba
Jonas Beckeman <jonas.beckeman@gmail.com>
2021-12-12 16:09:58 +0100

    add tools, FSharp.Core 6.0.1

---
+++ Tools/AofCSiteInfo.fs
@@ -0,0 +1,10 @@
+﻿namespace Tools
+
+module AofCSiteInfo =
+
+    let main =
+        let year = 2021
+        let day = 12
+        let url = $"https://adventofcode.com/{year}/{day}"
+        //let client = new HttpClient
+        0
