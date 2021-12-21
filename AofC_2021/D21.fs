69875073d141a638e600e3a9e612198975c4ff2f
Jonas Beckeman <jonas.beckeman@gmail.com>
2021-12-21 21:25:47 +0100

    move out "optimized"

--- AofC_2021/D21.fs
+++ AofC_2021/D21.fs
@@ -48,22 +48,13 @@ let permutations depth aList =
     }
     loop 1 [] |> Seq.toList
 
-let part2 input = 
-    let data = parseInput input
-    let players = data |> Array.map(fun (_, pos) -> pos) |> Array.toList
-
-    let diracDieSides = 3
-    let numThrowsPerTurn = 3
-
-    let allCombosPerTurn = permutations numThrowsPerTurn [1..diracDieSides]
-    let countsBySum = Map (allCombosPerTurn |> List.groupBy (fun f -> f |> List.sum) |> List.map (fun (sum, org) -> (sum, org.Length)))
-    let maxSumPerTurn = countsBySum.Keys |> Seq.max
-    let winAt = 21
-
+let optimizedButBadly players (countsBySum: Map<int, int>) winAt =
     // attempt to optimize - if other player can reach goal in next turn, maybe we can calculate the rest more quickly..?
-    let rec loop2 turn numUniverses (players: Player list) sumExclude = seq {
-            let cbs = countsBySum |> Seq.filter (fun kv -> sumExclude |> List.contains kv.Key = false)
-            for outcome in cbs do
+    // Seems to take twice as long...
+    let maxSumPerTurn = countsBySum.Keys |> Seq.max
+
+    let rec loop2 turn numUniverses (players: Player list) countsBySumToUse = seq {
+            for (outcome: System.Collections.Generic.KeyValuePair<int,int>) in countsBySumToUse do
                 let (newP, modified) = update (turn % 2) outcome.Key players
                 if newP.Score >= winAt then
                     let winner = if modified[0].Score > modified[1].Score then 0 else 1
@@ -83,12 +74,25 @@ let part2 input =
                         else None
                         
                     if directCombos.IsSome then 
-                        let productCombos = directCombos.Value |> List.map(fun kv -> kv.Value) |> Seq.reduce (fun p c -> p * c)
+                        let productCombos = directCombos.Value |> Seq.map(fun kv -> kv.Value) |> Seq.reduce (fun p c -> p * c)
                         yield (otherIndex, numUniverses * (uint64 productCombos))
                         let toExclude = directCombos.Value |> Seq.map(fun kv -> kv.Key) |> Seq.toList
-                        yield! loop2 (turn + 1) (numUniverses * (uint64 outcome.Value)) modified toExclude
-                    else yield! loop2 (turn + 1) (numUniverses * (uint64 outcome.Value)) modified []
+                        let toExplore = countsBySum |> Seq.filter (fun kv -> toExclude |> List.contains kv.Key = false)
+                        yield! loop2 (turn + 1) (numUniverses * (uint64 outcome.Value)) modified toExplore
+                    else yield! loop2 (turn + 1) (numUniverses * (uint64 outcome.Value)) modified countsBySum
         }
+    loop2 0 1UL players countsBySum
+
+let part2 input = 
+    let data = parseInput input
+    let players = data |> Array.map(fun (_, pos) -> pos) |> Array.toList
+
+    let diracDieSides = 3
+    let numThrowsPerTurn = 3
+
+    let allCombosPerTurn = permutations numThrowsPerTurn [1..diracDieSides]
+    let countsBySum = Map (allCombosPerTurn |> List.groupBy (fun f -> f |> List.sum) |> List.map (fun (sum, org) -> (sum, org.Length)))
+    let winAt = 21
 
     let rec loop turn numUniverses (players: Player list) = seq {
             for outcome in countsBySum do
@@ -100,13 +104,9 @@ let part2 input =
                     yield! loop (turn + 1) (numUniverses * (uint64 outcome.Value)) modified
         }
 
-    //let seq = loop2 0 1UL players []
     let seq = loop 0 1UL players
 
-    let stopWatch = new Stopwatch();
-    stopWatch.Start()
-    let xxx = seq //|> Seq.take 1000000
-                    |> Seq.fold (fun (agg: uint64 list) (winner, numUniverses) ->
+    let xxx = seq |> Seq.fold (fun (agg: uint64 list) (winner, numUniverses) ->
                                                     let ulWinner = uint64 winner
                                                     let newWins = [
                                                         agg[0] + ((1UL - ulWinner) * numUniverses);
@@ -115,8 +115,5 @@ let part2 input =
                                                     newWins
                                                     ) [0UL; 0UL;]
     let result = xxx |> List.max
-    
-    stopWatch.Stop()
-    printfn $"{stopWatch.Elapsed.TotalSeconds}"
     
     result
